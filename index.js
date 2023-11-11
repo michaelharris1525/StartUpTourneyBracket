113523f2917164489dbff24712926cc1402d1c6e
Your Name <michaelharris1525@gmail.com>
2023-11-11 16:24:22 -0700

    index.js

--- index.js
+++ index.js
@@ -1,59 +1,110 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
 
-// The service port. In production the front-end code is statically hosted by the service on the same port.
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+app.use(express.json())
 
-// JSON body parsing using built-in middleware
-app.use(express.json());
+const users = []
 
-// Serve up the front-end static content hosting
-app.use(express.static('public'));
+app.get('/users', (req, res) => {
+  res.json(users)
+})
 
-// Router for service endpoints
-var apiRouter = express.Router();
-app.use(`/api`, apiRouter);
+app.post('/users', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const user = { name: req.body.name, password: hashedPassword }
+    users.push(user)
+    res.status(201).send()
+  } catch {
+    res.status(500).send()
+  }
+})
 
-// GetScores
-apiRouter.get('/scores', (_req, res) => {
-  res.send(scores);
-});
-
-// SubmitScore
-apiRouter.post('/score', (req, res) => {
-  scores = updateScores(req.body, scores);
-  res.send(scores);
-});
-
-// Return the application's default page if the path is unknown
-app.use((_req, res) => {
-  res.sendFile('index.html', { root: 'public' });
-});
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
-// updateScores considers a new score for inclusion in the high scores.
-// The high scores are saved in memory and disappear whenever the service is restarted.
-let scores = [];
-function updateScores(newScore, scores) {
-  let found = false;
-  for (const [i, prevScore] of scores.entries()) {
-    if (newScore.score > prevScore.score) {
-      scores.splice(i, 0, newScore);
-      found = true;
-      break;
+app.post('/users/login', async (req, res) => {
+  const user = users.find(user => user.name === req.body.name)
+  if (user == null) {
+    return res.status(400).send('Cannot find user')
+  }
+  try {
+    if(await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Success')
+    } else {
+      res.send('Not Allowed')
     }
+  } catch {
+    res.status(500).send()
   }
+})
 
-  if (!found) {
-    scores.push(newScore);
-  }
+app.listen(3000)
 
-  if (scores.length > 10) {
-    scores.length = 10;
-  }
 
-  return scores;
-}
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+
+// // The service port. In production the front-end code is statically hosted by the service on the same port.
+// const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// // JSON body parsing using built-in middleware
+// app.use(express.json());
+
+// // Serve up the front-end static content hosting
+// app.use(express.static('public'));
+
+// // Router for service endpoints
+// var apiRouter = express.Router();
+// app.use(`/api`, apiRouter);
+
+// // GetScores
+// apiRouter.get('/scores', (_req, res) => {
+//   res.send(scores);
+// });
+
+// // SubmitScore
+// apiRouter.post('/score', (req, res) => {
+//   scores = updateScores(req.body, scores);
+//   res.send(scores);
+// });
+
+// // Return the application's default page if the path is unknown
+// app.use((_req, res) => {
+//   res.sendFile('index.html', { root: 'public' });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`);
+// });
+
+// // updateScores considers a new score for inclusion in the high scores.
+// // The high scores are saved in memory and disappear whenever the service is restarted.
+// let scores = [];
+// function updateScores(newScore, scores) {
+//   let found = false;
+//   for (const [i, prevScore] of scores.entries()) {
+//     if (newScore.score > prevScore.score) {
+//       scores.splice(i, 0, newScore);
+//       found = true;
+//       break;
+//     }
+//   }
+
+//   if (!found) {
+//     scores.push(newScore);
+//   }
+
+//   if (scores.length > 10) {
+//     scores.length = 10;
+//   }
+
+//   return scores;
+// }
